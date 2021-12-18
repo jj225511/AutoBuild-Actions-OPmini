165cf1f57d542cda9c70efb0f644f28b2cf3121b
jj225511 <admin@zyzh.cc>
2021-12-18 22:08:02 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -5,11 +5,11 @@
 Firmware_Diy_Core() {
 
 	Author=ZYZH
-	Author_URL=
+	Author_URL=https://github.com/jj225511
 	Default_IP="192.168.5.1"
-	Banner_Message="Powered by AutoBuild-Actions"
+	Banner_Message="Powered by ZYZH"
 
-	Short_Firmware_Date=true
+	Short_Firmware_Date=false
 	Checkout_Virtual_Images=false
 	Firmware_Format=AUTO
 	REGEX_Skip_Checkout="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory"
@@ -38,5 +38,20 @@ Firmware_Diy() {
 	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages
 	# ${BASE_FILES}			俗称替换大法的路径, 位于 package/base-files/files
 
-	:
+	case "${OP_Maintainer}/${OP_REPO_NAME}:${OP_BRANCH}" in
+	coolsnowwolf/lede:master)
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${feeds_pkgs}/ttyd/files/ttyd.config
+	;;
+	esac
+
+	case "${TARGET_PROFILE}" in
+	d-team_newifi-d2)
+		patch -i ${CustomFiles}/mac80211_d-team_newifi-d2.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+		Copy ${CustomFiles}/system_d-team_newifi-d2 ${base_files}/etc/config system
+		#sed -i 's/OpenWrt/ZYZH-Router/g' package/base-files/files/bin/config_generate
+		[[ ${OP_REPO_NAME} == lede ]] && sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+	;;
+	esac
+        sed -i 's/OpenWrt/ZYZH-Router/g' package/base-files/files/bin/config_generate
+	sed -i 's/OpenWrt/ZYZH-Router/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 }
